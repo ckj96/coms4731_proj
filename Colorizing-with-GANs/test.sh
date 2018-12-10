f9e81b7fcdad59951ff1ee59f9fba45070a50ad4
ckj96 <ckj96123@gmail.com>
2018-12-10 14:44:26 -0500

    minor modification

--- Colorizing-with-GANs/test.sh
+++ Colorizing-with-GANs/test.sh
@@ -1,3 +1,3 @@
-python3 test_on_data.py  --name in1_b \
+python3 test_on_data.py  --name apple \
 	--dataset self \
-	--resume_checkpoint /home/cc4192/coms4995_colorization/Colorizing-with-GANs/checkpoints/self/in1_b/ \
+	--resume_checkpoint /home/cc4192/coms4995_colorization/Colorizing-with-GANs/checkpoints/self/apple/ \
--- Colorizing-with-GANs/train.sh
+++ Colorizing-with-GANs/train.sh
@@ -1,4 +1,4 @@
-python3 train.py  --name in1_b \
+python3 train.py  --name man \
 	--dataset self \
 	--resume_checkpoint /home/cc4192/coms4995_colorization/Colorizing-with-GANs/checkpoints/self/ \
 	--save-interval 900 \
